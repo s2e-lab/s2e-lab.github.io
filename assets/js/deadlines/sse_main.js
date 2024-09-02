7ec536b28245cfec6076e3b2b59191e54616facc
Joanna Cecilia da Silva Santos <jc_joanna@yahoo.com.br>
2024-09-02 10:40:11 -0400

    Added improved deadlines

--- assets/js/deadlines/sse_main.js
+++ assets/js/deadlines/sse_main.js
@@ -3,75 +3,82 @@
 $(function() {
   deadlineByConf = {};
 	
-  {% for conf in site.data.deadlines.security_deadlines %}
-  
-  {% if conf.deadline[0] == "TBA" %}
-  {% assign conf_id = conf.name | append: conf.year | append: '-0' | slugify %}
-  $('#{{ conf_id }} .timer').html("TBA");
-  $('#{{ conf_id }} .deadline-time').html("TBA");
-  deadlineByConf["{{ conf_id }}"] = null;
+  {% assign all_conferences = site.data.deadlines %}
 
-  {% else %}
-  var rawDeadlines = {{ conf.deadline | jsonify }} || [];
-  if (rawDeadlines.constructor !== Array) {
-    rawDeadlines = [rawDeadlines];
-  }
-  var parsedDeadlines = [];
-  while (rawDeadlines.length > 0) {
-    var rawDeadline = rawDeadlines.pop();
-    // deal with year template in deadline
-    year = {{ conf.year }};
-    rawDeadline = rawDeadline.replace('%y', year).replace('%Y', year - 1);
-    // adjust date according to deadline timezone
-    {% if conf.timezone %}
-    var deadline = moment.tz(rawDeadline, "{{ conf.timezone }}");
+
+  {% for file in site.data.deadlines %}
+  {% if file.first contains '_deadlines' %}
+  {% for conf in  file[1] %}
+    // alert("{{conf.name}}")
+    {% if conf.deadline[0] == "TBA" %}
+      {% assign conf_id = conf.name | append: conf.year | append: '-0' | slugify %}
+      $('#{{ conf_id }} .timer').html("TBA");
+      $('#{{ conf_id }} .deadline-time').html("TBA");
+      deadlineByConf["{{ conf_id }}"] = null;
+
     {% else %}
-    var deadline = moment.tz(rawDeadline, "Etc/GMT+12"); // Anywhere on Earth
-    {% endif %}
+      var rawDeadlines = {{ conf.deadline | jsonify }} || [];
+      if (rawDeadlines.constructor !== Array) {
+        rawDeadlines = [rawDeadlines];
+      }
+      var parsedDeadlines = [];
+      while (rawDeadlines.length > 0) {
+        var rawDeadline = rawDeadlines.pop();
+        // deal with year template in deadline
+        year = {{ conf.year }};
+        rawDeadline = rawDeadline.replace('%y', year).replace('%Y', year - 1);
+        // adjust date according to deadline timezone
+        {% if conf.timezone %}
+          var deadline = moment.tz(rawDeadline, "{{ conf.timezone }}");
+        {% else %}
+          var deadline = moment.tz(rawDeadline, "Etc/GMT+12"); // Anywhere on Earth
+        {% endif %}
 
-    // post-process date
-    if (deadline.minutes() === 0) {
-      deadline.subtract(1, 'seconds');
-    }
-    if (deadline.minutes() === 59) {
-      deadline.seconds(59);
-    }
-    parsedDeadlines.push(deadline);
-  }
-  // due to pop before; we need to reverse such that the i index later matches
-  // the right parsed deadline
-  parsedDeadlines.reverse();
-
-  {% assign range_end = conf.deadline.size | minus: 1 %}
-  {% for i in (0..range_end) %}
-  {% assign conf_id = conf.name | append: conf.year | append: '-' | append: i | slugify %}
-  var deadlineId = {{ i }};
-  if (deadlineId < parsedDeadlines.length) {
-    var confDeadline = parsedDeadlines[deadlineId];
-
-    // render countdown timer
-    if (confDeadline) {
-      function make_update_countdown_fn(confDeadline) {
-        return function(event) {
-          diff = moment() - confDeadline
-          if (diff <= 0) {
-             $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
-          } else {
-            $(this).html(confDeadline.fromNow());
-          }
+        // post-process date
+        if (deadline.minutes() === 0) {
+          deadline.subtract(1, 'seconds');
         }
+        if (deadline.minutes() === 59) {
+          deadline.seconds(59);
+        }
+        parsedDeadlines.push(deadline);
       }
-      $('#{{ conf_id }} .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
-      // check if date has passed, add 'past' class to it
-      if (moment() - confDeadline > 0) {
-        $('#{{ conf_id }}').addClass('past');
-      }
-      $('#{{ conf_id }} .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
-      deadlineByConf["{{ conf_id }}"] = confDeadline;
-    }
-  } else {
-    // TODO: hide the conf_id ?
-  }
+      // due to pop before; we need to reverse such that the i index later matches
+      // the right parsed deadline
+      parsedDeadlines.reverse();
+
+      {% assign range_end = conf.deadline.size | minus: 1 %}
+      {% for i in (0..range_end) %}
+        {% assign conf_id = conf.name | append: conf.year | append: '-' | append: i | slugify %}
+        var deadlineId = {{ i }};
+        if (deadlineId < parsedDeadlines.length) {
+          var confDeadline = parsedDeadlines[deadlineId];
+
+          // render countdown timer
+          if (confDeadline) {
+            function make_update_countdown_fn(confDeadline) {
+              return function(event) {
+                diff = moment() - confDeadline
+                if (diff <= 0) {
+                   $(this).html(event.strftime('%D days %Hh %Mm %Ss'));
+                } else {
+                  $(this).html(confDeadline.fromNow());
+                }
+              }
+            }
+            $('#{{ conf_id }} .timer').countdown(confDeadline.toDate(), make_update_countdown_fn(confDeadline));
+            // check if date has passed, add 'past' class to it
+            if (moment() - confDeadline > 0) {
+              $('#{{ conf_id }}').addClass('past');
+            }
+            $('#{{ conf_id }} .deadline-time').html(confDeadline.local().format('D MMM YYYY, h:mm:ss a'));
+            deadlineByConf["{{ conf_id }}"] = confDeadline;
+          }
+        } else {
+          // TODO: hide the conf_id ?
+        }
+      {% endfor %}
+    {% endif %}    
   {% endfor %}
   {% endif %}
   {% endfor %}
